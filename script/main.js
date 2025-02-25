c88628c025bb976e3a5fe5d0e2f29289042303a8
mdrahimultahsin <mdrahimultahsin@gmail.com>
2025-02-25 21:57:41 +0600

    added cashout function

--- script/main.js
+++ script/main.js
@@ -8,23 +8,25 @@ document.getElementById("logout-btn").addEventListener("click", function () {
 const cards = document.querySelectorAll(".card");
 for (const card of cards) {
   card.addEventListener("click", function () {
-    for(const otherCard of cards){
-        otherCard.classList.remove('card-active');
+    for (const otherCard of cards) {
+      otherCard.classList.remove("card-active");
     }
-    card.classList.add("card-active");   
-});  
-};
+    card.classList.add("card-active");
+  });
+}
 
 /**
  * Add Money Function
  */
 
 // Section Show
-document.getElementById("add-money-card").addEventListener("click", function () {
-  const addMoneyContainer = document.getElementById("add-money-container");
-  addMoneyContainer.classList.remove("hidden");
-  document.getElementById('cashout-container').classList.add('hidden');
-});
+document
+  .getElementById("add-money-card")
+  .addEventListener("click", function () {
+    const addMoneyContainer = document.getElementById("add-money-container");
+    addMoneyContainer.classList.remove("hidden");
+    document.getElementById("cashout-container").classList.add("hidden");
+  });
 
 // Functionalities
 document
@@ -55,16 +57,41 @@ document
     }
   });
 
-
-  /**
+/**
  * CashOut Function
  */
 
 // Section Show
 document.getElementById("cashout-card").addEventListener("click", function () {
-    const cashoutContainer = document.getElementById("cashout-container");
-    cashoutContainer.classList.remove("hidden");
-    document.getElementById('add-money-container').classList.add('hidden');
+  const cashoutContainer = document.getElementById("cashout-container");
+  cashoutContainer.classList.remove("hidden");
+  document.getElementById("add-money-container").classList.add("hidden");
+});
+
+// Functionalities
+document
+  .getElementById("withdraw-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const numberInput = document.querySelector("#number-input").value;
+    const pinInput = document.querySelector("#pin-input").value;
+    const pinValue = Number(pinInput);
+    let mainBalance = document.getElementById("main-balance").innerText;
+    const mainBalanceValue = parseFloat(mainBalance);
+    const cashoutAmount = document.getElementById("cashout-amount").value;
+    const cashoutAmountValue = parseFloat(cashoutAmount);
+    if (numberInput.length === 11 && numberInput.startsWith("01")) {
+      if (pinValue === 1234) {
+        if (mainBalanceValue > 0) {
+          let sum = mainBalanceValue - cashoutAmountValue;
+          sum >=0?document.getElementById("main-balance").innerText = sum:alert("Insufficient Balance");
+        } else {
+          alert("Insufficient Balance");
+        }
+      } else {
+        alert("Wrong pin");
+      }
+    } else {
+      alert("Invalid Phone Number");
+    }
   });
-  
-  // Functionalities
