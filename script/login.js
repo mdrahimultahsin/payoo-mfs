906934394fb34c9ee14f6ea58996e9e957f1fd2d
mdrahimultahsin <mdrahimultahsin@gmail.com>
2025-02-26 20:43:36 +0600

    new update

--- script/login.js
+++ script/login.js
@@ -1,16 +1,16 @@
-document.getElementById('login-btn').addEventListener('click',function(event){
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-    const phone = document.getElementById('phone-input').value;
-    const pin = getInputValueById('pin-input');
-    if(phone.length === 11 && phone.startsWith('01')){
-        if(pin ===1234){
-            window.location.href = '../main.html'
-        }
-        else{
-            alert('Wrong pin');
-        }
+    const phone = document.getElementById("phone-input").value;
+    const pin = getInputValueById("pin-input");
+    if (phone.length === 11 && phone.startsWith("01")) {
+      if (pin === 1234) {
+        window.location.href = "main.html";
+      } else {
+        alert("Wrong pin");
+      }
+    } else {
+      alert("Invalid Phone Number");
     }
-    else{
-        alert('Invalid Phone Number')
-    }
-});
+  });
